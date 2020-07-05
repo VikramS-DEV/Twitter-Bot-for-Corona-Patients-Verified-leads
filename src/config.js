1ecafebec162a9cc7acb69435c223810fb93e2d3
AntechMicroSystems <vikrambest1@gmail.com>
2020-07-05 15:39:45 +0530

    Retweet and likes

---
+++ src/config.js
@@ -0,0 +1,13 @@
+//config.js
+/** TWITTER APP CONFIGURATION
+ * consumer_key
+ * consumer_secret
+ * access_token
+ * access_token_secret
+ */
+module.exports = {
+  consumer_key: "Hd7V2EKNiNjAmYGrPUUjrm6rp",
+  consumer_secret: "LZOhqgJ9lXXW8ZbWAXtxM6JkRrfrqCEajKUVNQVyGcZ6v9HL4j",
+  access_token_key: "1279427017050451968-84EZta1tWLvFkFkzmbZMWXxxnrAtjD",
+  access_token_secret: "LMpgemNCbcMdDiAA7PsnxkUJAH7HdaIJm1UaWA4ItvuVd"
+  }
